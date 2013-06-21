d3d1805da5ac0e5e86e72908321cd10857809a1f
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-06-21 13:52:32 +0100

    Evil parenthesis.

--- bpnn.hs
+++ bpnn.hs
@@ -7,13 +7,16 @@ import Data.List
 import Control.Monad.State
 import Control.Monad.Writer
 import Control.Arrow
+import Control.Applicative ((<$>))
 import Debug.Trace
 
-data NeuralNetwork = NeuralNetwork Nodes Nodes Nodes deriving (Show)
+data NeuralNetwork = NeuralNetwork Nodes Nodes Nodes deriving Show
 
-data Node = Node Activation Weights Momentums -- TODO make record syntax for automatic getter
-instance Show Node where
-  show (Node a w m) = "Node {\n\tActivation : " ++ show a ++ "\n\tWeights : " ++ show w ++ "\n\tMomemtums : " ++ show m ++ "\n}\n"
+data Node = Node { activation :: Activation
+                 , weights    :: Weights
+                 , momentums  :: Momentums
+                 } deriving Show
+
 type Nodes = [Node]
 type Activation = Double
 type Weight = Double
@@ -29,17 +32,6 @@ type Patterns = [Pattern]
 type RandomState a = State StdGen a
 type NeuralNetworkState = (NeuralNetwork, Double)
 
------------------------------------
--- * Field getters
-----------------------------------
-getActivation :: Node -> Activation
-getActivation (Node a _ _) = a
-
-getWeights :: Node -> Weights
-getWeights (Node _ w _) = w
-
-getMomemtums :: Node -> Momentums
-getMomemtums (Node _ _ m) = m
 
 -----------------------------------
 -- * Main
@@ -53,7 +45,7 @@ main = do
                      ,([0,1],[1])
                      ,([1,0],[1])
                      ,([1,1],[0])]
-  let (nn', e) = execState (trainFor xor_patterns 0.5 0.1 1000) (nn, 0)
+  let (nn', _) = execState (trainFor xor_patterns 0.5 0.1 1000) (nn, 0)
   let results = test nn' xor_patterns
   mapM_ print results
 
@@ -62,94 +54,83 @@ main = do
 ----------------------------------
 
 test :: NeuralNetwork -> Patterns -> [String]
-test nn [] = []
-test nn ps = fmap (testPattern nn) ps
+test _ [] = []
+test nn ps = testPattern nn <$> ps
 
 testPattern :: NeuralNetwork -> Pattern -> String
-testPattern nn p =
-  "Input : " ++ show i ++ " -> " ++ show (map getActivation outputNodes)
-    where (i, o) = p
-          (nn', _) = execState (update i) (nn, 0)
-          (NeuralNetwork _ _ outputNodes) = nn'
+testPattern nn (i, _) =
+  "Input : " ++ show i ++ " -> " ++ show (map activation outputNodes)
+    where (NeuralNetwork _ _ outputNodes, _) = execState (update i) (nn, 0)
+
 ----------------------------------
 -- * Update
 ----------------------------------
 
 trainFor :: Patterns -> Double -> Double -> Int -> State NeuralNetworkState ()
-trainFor pats learn_rate delta_p iterations = do
-  (nn, e) <- get
-  let (nn', e') = last $ take iterations $ iterateS (train pats learn_rate delta_p) (nn, e)
-  put (nn', e')
-  return ()
+trainFor pats learn_rate delta_p iterations =
+  get >>= put . last . take iterations . iterateS (train pats learn_rate delta_p)
 
 train :: Patterns -> Double -> Double -> State NeuralNetworkState ()
 train [] _ _ = return ()
 train ps learn_rate delta_p = do
-  (nn, e) <- get
-  let (nn', e') = foldl (\acc x -> execState (trainPattern x learn_rate delta_p) acc) (nn, e) ps
-  put (nn', e')
-  return ()
+  t <- get
+  put $ foldl (\acc x -> execState (trainPattern x learn_rate delta_p) acc) t ps
 
 trainPattern :: Pattern -> Double -> Double -> State NeuralNetworkState ()
 trainPattern p learn_rate delta_p = do
-  (nn, e) <- get
-  let (nn_u, _ ) = execState (update $ fst p) (nn, e)
+  t@(nn, e) <- get
+  let (nn_u, _) = execState (update $ fst p) t
   let (nn' , e') = execState (backPropagate (snd p) learn_rate delta_p) (nn_u, e)
-  trace (show nn') $ put (nn' , e')
-  return ()
+  traceShow nn' $ put (nn' , e') -- Stop.
 
 update :: PatternInput -> State NeuralNetworkState ()
 update inputs = do
-  (nn, e) <- get
-  let (NeuralNetwork inputNodes hiddenNodes outputNodes) = nn
+  (NeuralNetwork inputNodes hiddenNodes outputNodes, e) <- get
   let inputNodes' = applyInput inputs inputNodes
   let hiddenActivations = generateNextActivations inputNodes
-  let hiddenNodes' = fmap(\(Node _ w m, a) -> Node a w m) $ zip hiddenNodes hiddenActivations
+  let hiddenNodes' = zwa hiddenNodes hiddenActivations
   let outputActivations = generateNextActivations hiddenNodes
-  let outputNodes' = fmap(\(Node _ w m, a) -> Node a w m) $ zip outputNodes outputActivations
-  let nn' = NeuralNetwork inputNodes hiddenNodes' outputNodes'
-  put (nn', e)
-  return ()
+  let outputNodes' = zwa outputNodes outputActivations
+  put (NeuralNetwork inputNodes hiddenNodes' outputNodes', e)
+  where zwa = zipWith (\n a -> n { activation = a })
 
 applyInput :: PatternInput -> Nodes -> Nodes
 applyInput inputs inputNodes =
-  inputNodes' ++ [biasNode]
-    where biasNode = last inputNodes
-          inputNodes' = fmap(\(Node a w m, a') -> (Node a' w m)) $ zip inputNodes inputs
+  inputNodes' ++ [last inputNodes]
+    where inputNodes' = zipWith (\n a' -> n { activation = a' }) inputNodes inputs
 
 generateNextActivations :: Nodes -> [Activation]
 generateNextActivations = fmap generateNextActivation
 
 generateNextActivation :: Node -> Activation
-generateNextActivation (Node a w _) = sigmoid $ sum $ fmap (*a) w
+generateNextActivation (Node a w _) = sigmoid . sum $ (* a) <$> w
 
 backPropagate :: PatternOutput -> Double -> Double -> State NeuralNetworkState ()
 backPropagate outputs learn_rate delta_p = do
-  (nn, e) <- get
-  let (NeuralNetwork inputNodes hiddenNodes outputNodes) = nn
-  let output_errors = zipWith (-) (map getActivation outputNodes) outputs
-  let output_deltas = (map dsigmoid >>> zipWith(*)) (map getActivation outputNodes) output_errors
-  let hidden_errors = fmap (\(Node _ w _) -> sum $ zipWith (*) output_deltas w) hiddenNodes -- TODO
-  let hidden_deltas = (map dsigmoid >>> zipWith(*)) (map getActivation hiddenNodes) hidden_errors
-  let hiddenNodes'  = backPropagateResults output_deltas hiddenNodes learn_rate delta_p
-  let inputNodes'   = backPropagateResults hidden_deltas inputNodes learn_rate delta_p
-  let nn' = NeuralNetwork inputNodes' hiddenNodes' outputNodes
-  let e' = foldl (\acc x -> acc + 0.5 * x ** 2) 0.0 $ zipWith (-) outputs (map getActivation outputNodes)
-  put (nn', e')
-  return ()
+  (NeuralNetwork inputNodes hiddenNodes outputNodes, _) <- get
+  let output_errors = zipWith (-) (mapA outputNodes) outputs
+  let output_deltas = mDSigAct outputNodes output_errors
+  let hidden_errors = sum . zipWith (*) output_deltas . weights <$> hiddenNodes
+  let hidden_deltas = mDSigAct hiddenNodes hidden_errors
+  let hiddenNodes'  = propRes output_deltas hiddenNodes
+  let inputNodes'   = propRes hidden_deltas inputNodes
+  let e' = foldl (\acc x -> acc + 0.5 * x ** 2.0) 0.0 $ zipWith (-) outputs $ mapA outputNodes
+  put (NeuralNetwork inputNodes' hiddenNodes' outputNodes, e')
+  where propRes x y  = backPropagateResults x y learn_rate delta_p
+        mapA         = map activation
+        mDSigAct x   = map dsigmoid >>> zipWith (*) $ mapA x
 
 backPropagateResults :: [Double] -> Nodes -> Double -> Double -> Nodes
 backPropagateResults deltas nodes learn_rate delta_p =
-  fmap (\x -> backPropagateResult deltas x learn_rate delta_p) nodes
+  (\x -> backPropagateResult deltas x learn_rate delta_p) <$> nodes
 
 backPropagateResult :: [Double] -> Node -> Double -> Double -> Node
-backPropagateResult deltas node learn_rate delta_p =
-  Node a w' m'
-    where (Node a w m) = node
-          changes = fmap (* a) deltas
+backPropagateResult deltas (Node a w m) learn_rate delta_p =
+  Node a w' changes
+    where changes = (* a) <$> deltas
           -- curWeight + learnRate * change + momentum * curMomentum
-          w' = (map (* delta_p) >>> zipWith(+)) m $ (map (* learn_rate) >>> zipWith(+)) changes w;
-          m' = changes
+          w' = mzw delta_p m $ mzw learn_rate changes w
+          mzw x = map (* x) >>> zipWith (+)
 
 ----------------------------------
 -- * Init
@@ -163,34 +144,24 @@ initNetwork i' h o gen = do
   NeuralNetwork iLayer hLayer oLayer
 
 initNodeLayer :: Int -> Int -> (Double, Double) -> RandomState Nodes
-initNodeLayer ni no bnds = mapM (\x -> initNode no bnds) [1..ni]
+initNodeLayer ni no bnds = mapM (const $ initNode no bnds) [1 .. ni]
 
 initNode :: Int -> (Double, Double) -> RandomState Node
-initNode n bnds = do
-  let a = 1.0
-  w <- randList bnds n
-  let m = replicate n 0.0
-  return (Node a w m)
+initNode n bnds = randList bnds n >>= \w -> return $ Node 1.0 w $ replicate n 0.0
 
 ----------------------------------
 -- * Util
 ----------------------------------
 
 getBoundedRandom :: Random a => (a,a) -> RandomState a
-getBoundedRandom bnds = do
-  gen <- get
-  let (val, gen') = randomR bnds gen
-  put gen'
-  return val
+getBoundedRandom bnds = get >>= putRet . randomR bnds
 
 runBoundedRandom :: Random a => (a,a) -> RandomState a
-runBoundedRandom bnds = do
-  oldState <- get
-  let (result, newState) = runState (getBoundedRandom bnds) oldState
-  put newState
-  return result
+runBoundedRandom bnds = get >>= putRet . runState (getBoundedRandom bnds)
 
--- Why can't this be mapM(const runBoundedRandom bnds) ?
+putRet :: MonadState s m => (b, s) -> m b
+putRet (r, s) = put s >> return r
+
 randList :: Random a => (a,a) -> Int -> RandomState [a]
 randList bnds n = mapM (const $ runBoundedRandom bnds) [1..n]
 
